ba635d61723b7027eef227358331a395c495ff35
Jonas Knarbakk <jonas@knarbakk.no>
2017-01-30 00:54:51 +0100

    Preperations for lecture 4

--- Forelesning3/Oppgave3/src/Person.cpp
+++ Forelesning3/Oppgave3/src/Person.cpp
@@ -1,32 +1,81 @@
 #include "Person.hpp"
 
 Person::Person(){
-    m_PNumber = "Undefined";
     m_Name = "Undefined";
     m_Adress = "Undefined";
+    m_PNumber = "Undefined";
 }
 
-Person::Person(std::string pNumber, std::string name, std::string adress){
-    m_PNumber = pNumber;
+Person::Person(std::string name, std::string adress, std::string pNumber){
     m_Name = name;
     m_Adress = adress;
+    m_PNumber = pNumber;
 }
 
 Person::~Person(){
 }
 
-std::string Person::get_pnumber() const{
-    return m_PNumber;
+void Person::set_name(std::string name){
+    m_Name = name;
 }
 
 std::string Person::get_name() const{
     return m_Name;
 }
 
+void Person::set_adress(std::string adress){
+    m_Adress = adress;
+}
+
 std::string Person::get_adress() const{
     return m_Adress;
 }
 
+void Person::set_pnumber(std::string pnumber){
+    m_PNumber = pnumber;
+}
+
+std::string Person::get_pnumber() const{
+    return m_PNumber;
+}
+
 std::string Person::get_data() const{
-    return "Person Number: " + m_PNumber + " Name: " + m_Name + " Adress: " + m_Adress;
+    return "Name: " + m_Name + " Adress: " + m_Adress + " Person Number: " + m_PNumber;
+}
+
+bool operator ==(const Person &personOne, const Person &personTwo){
+    return personOne.get_pnumber() == personTwo.get_pnumber();
+}
+
+bool operator <(const Person &personOne, const Person &personTwo){
+    return personOne.get_pnumber() < personTwo.get_pnumber();    
+}
+
+bool operator >(const Person &personOne, const Person &personTwo){
+    return personOne.get_pnumber() > personTwo.get_pnumber();    
+}
+
+std::ostream &operator <<(std::ostream &output, const Person &person){
+    output << "Navn: " << person.get_name()
+        << "\nAdresse: " << person.get_adress()
+        << "\nPersonnummer: " << person.get_pnumber()
+        << std::endl
+        << std::endl;
+
+    return output;
+}
+
+std::istream &operator >>(std::istream &input, Person &person){
+    std::string name, adress, pnumber;
+    std::cout << "Navn: ";
+    std::cin >> name;
+    std::cout << "Adresse: ";
+    std::cin >> adress;
+    std::cout << "Personnnummer: ";
+    std::cin >> pnumber;
+    person.set_name(name);
+    person.set_adress(adress);
+    person.set_pnumber(pnumber);
+
+    return input;
 }
--- Forelesning3/Oppgave3/src/Person.hpp
+++ Forelesning3/Oppgave3/src/Person.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include <iostream>
 
 class Person {
     private:
@@ -12,8 +13,21 @@ class Person {
         Person(std::string, std::string, std::string);
         ~Person();
 
+        void set_pnumber(std::string pnumber);
         std::string get_pnumber() const;
+        void set_name(std::string name);
         std::string get_name() const;
+        void set_adress(std::string adress);
         std::string get_adress() const;
         std::string get_data() const;
+        // Overloading of the == operator to make a rule for comparison
+        friend bool operator ==(const Person &, const Person &);
+        // Overloading of the < operator to make a rule for comparison
+        friend bool operator <(const Person &, const Person &);
+        // Overloading of the > operator to make a rule for comparison
+        friend bool operator >(const Person &, const Person &);
+        // Overloading of the << operator to make a rule for printing output via cout
+        friend std::ostream &operator <<(std::ostream &, const Person &);
+        // Overloading of the >> operator to make a rule for taking input via cin
+        friend std::istream &operator >>(std::istream &, Person &);
 };
